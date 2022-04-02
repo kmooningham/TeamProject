fe9c23eb2696672f6d582e15389163a151e96205
smccullarUCA <72277408+smccullarUCA@users.noreply.github.com>
2022-04-02 13:46:31 -0500

    Basic window control added

--- TeamProject/client/BoardController.java
+++ TeamProject/client/BoardController.java
@@ -5,6 +5,26 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class BoardController implements ActionListener {
+	private JPanel container;
+	
+	public BoardController(JPanel container) {
+		this.container = container;
+	}
+	
 	public void actionPerformed(ActionEvent ae) {
+		String command = ae.getActionCommand();
+		
+		/* command control here
+		if (command == "whatever") {		
+		
+		} else if (command == "some other command") {
+				
+		}
+		*/
+		
+		//set view to login panel
+		CardLayout cardLayout = (CardLayout)container.getLayout();
+		cardLayout.show(container,  "login");
+		
 	}
 }
--- TeamProject/client/LoginController.java
+++ TeamProject/client/LoginController.java
@@ -14,22 +14,16 @@ public class LoginController implements ActionListener {
 	public void actionPerformed(ActionEvent ae) {
 		String command = ae.getActionCommand();
 		
+		/* command control here
 		if (command == "Cancel") {
-			CardLayout cardLayout = (CardLayout)container.getLayout();
-			cardLayout.show(container,  "1");
+		
 		} else if (command == "Submit") {
-			LoginPanel loginPanel = (LoginPanel)container.getComponent(1);
-			//LoginData data = new LoginData(loginPanel.getUsername(), loginPanel.getPassword());
-			
-			//if (data.getUsername().equals("") || data.getPassword().equals("")) {
-			//	return;		
-			//}
-			
-			//System.out.println("Username added: " + data.getUsername().toString());
-			
-			CardLayout cardLayout = (CardLayout)container.getLayout();
-			cardLayout.show(container,  "1");
-			
+		
 		}
+		*/
+		
+		//set view to board panel
+		CardLayout cardLayout = (CardLayout)container.getLayout();
+		cardLayout.show(container,  "board");
 	}
 }
