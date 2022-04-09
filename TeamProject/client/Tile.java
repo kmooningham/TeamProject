5617ff7d1b52d595816500c3400fec25c3f11a7c
smccullarUCA <72277408+smccullarUCA@users.noreply.github.com>
2022-04-09 16:31:57 -0500

    Added icon functions

--- TeamProject/client/Tile.java
+++ TeamProject/client/Tile.java
@@ -3,6 +3,7 @@ package client;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Graphics2D;
 
 public class Tile extends JButton {
 	private int col;
@@ -13,11 +14,19 @@ public class Tile extends JButton {
 	
 	Tile(Tile[][] tiles, JPanel panel, int col, int row) {
 		//set name to coords
-		super(Integer.toString(col) + "," +  row);
+		//super(Integer.toString(col) + "," +  row);
+		
 		
 		this.panel = panel;
 		this.col = col;
 		this.row = row;
+		
+		//icon location
+		ImageIcon icon = new ImageIcon("checker.png");
+		//Resize icon by casting to "Icon" and scaling, then converting back to "ImageIcon"
+		ImageIcon scaledIcon = new ImageIcon(icon.getImage().getScaledInstance(75, 75, java.awt.Image.SCALE_SMOOTH));
+	//	this.setIcon(scaledIcon);
+		
 		
 		//text color
 		this.setForeground(Color.WHITE);
@@ -33,11 +42,12 @@ public class Tile extends JButton {
 		this.setBackground(bgColor);
 		//
 		//action listener for when the tile is clicked
+		
 		this.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				
-				
+				//example of functionality, set icon to piece texture when clicked
+				tiles[col][row].setIcon(scaledIcon);
 				
 				//example of action listener functionality, color diagonal tiles blue when clicked
 				for(int col = 0;col<8;col++) {
@@ -65,11 +75,15 @@ public class Tile extends JButton {
 				
 			}
 		});
-		
+
 	}
 	
 	public Color getBgColor() {
 		return bgColor;
 	}
 
+	//public void paint(Graphics g) {
+	//	Graphics2D g2d = (Graphics2D) g;
+	//	g2d.drawOval(0, 0, 10, 10);
+	//}
 }
